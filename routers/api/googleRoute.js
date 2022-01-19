6c792f9efdeccb4f908a3ec8bb3bba69a690cc60
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-19 18:28:08 +0200

    create google auth router

---
+++ routers/api/googleRoute.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const { asyncWrapper } = require('../../helpers')
+const {AuthController} = require('../../contollers')
+
+
+
+router.get('/google', asyncWrapper(AuthController.login))
+
+router.get('/google-redirect',  asyncWrapper(AuthController.redirect))
+
+
+module.exports = router
