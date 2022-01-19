728099c75c2dd8c5c3652a820ddda652f588e988
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-19 18:30:24 +0200

    google router re-export

--- routers/api/index.js
+++ routers/api/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./userRouter');
 const transactionRouter = require('./transactionRouter');
+const googleRouter = require('./googleRoute');
 
 module.exports = {
   userRouter,
   transactionRouter,
+  googleRouter,
 };
