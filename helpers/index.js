b387042bcb55d02c22dd730541d8a59c9fda34a7
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2022-01-20 20:33:11 +0200

    bag fix

--- helpers/index.js
+++ helpers/index.js
@@ -1,17 +1,14 @@
 const AppError = require('./errors');
 const { asyncWrapper, errorHandler } = require('./apiHelpers');
 const checkUserBalance = require('./checkUserBalance');
-const sumTransactionsValue = require('./sumTransactionsValue');
 const tokenService = require('./tokenHelpers');
 const transactionsData = require('./transactionsData');
-
 
 module.exports = {
   AppError,
   asyncWrapper,
   errorHandler,
   checkUserBalance,
-  sumTransactionsValue,
   tokenService,
   transactionsData,
 };
