ddabd575d667a39222d9234fd17d9fa59fe43903
Olha Dudchenko <dudchenko.olha92@gmail.com>
2022-01-21 21:41:11 +0200

    change tokens

--- services/users/getCurrentUser.js
+++ services/users/getCurrentUser.js
@@ -1,5 +1,5 @@
-const getCurrentUser = ({ _id, balance, email, tokenShort, tokenLong }) => {
-  const userData = { id: _id, email, balance, tokenShort, tokenLong };
+const getCurrentUser = ({ _id, balance, email, acces_token, refresh_token }) => {
+  const userData = { id: _id, email, balance, acces_token, refresh_token };
   return userData;
 };
 
