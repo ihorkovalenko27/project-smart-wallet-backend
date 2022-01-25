34540760dc0066bae6a5186dd5f693ab1d68730e
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2022-01-25 19:56:10 +0200

    update change balance

--- services/users/updateBalance.js
+++ services/users/updateBalance.js
@@ -1,11 +1,37 @@
-const { User } = require('../../models');
+const { User, Transaction } = require('../../models');
 
-const updateBalance = async ({ id, balance }) => {
-  const user = await User.findByIdAndUpdate(id, { balance }, { new: true });
+const updateBalance = async ({ user, balance }) => {
+  const dateNow = new Date();
+
+  const newTransaction = {
+    owner: user._id,
+    type: balance > user.balance ? 'income' : 'costs',
+    sum:
+      balance > user.balance ? balance - user.balance : user.balance - balance,
+    category: 'Прочее',
+    description: 'Изменение баланса пользователем',
+    day:
+      `${dateNow.getDay() + 1}`.length < 2
+        ? `0${dateNow.getDay() + 1}`
+        : `${dateNow.getDay() + 1}`,
+    month:
+      `${dateNow.getMonth() + 1}`.length < 2
+        ? `0${dateNow.getMonth() + 1}`
+        : `${dateNow.getMonth() + 1}`,
+    year: `${dateNow.getFullYear()}`,
+  };
+
+  await Transaction.create(newTransaction);
+  const updatedUser = await User.findByIdAndUpdate(
+    user._id,
+    { balance },
+    { new: true },
+  );
+
   const result = {
     id: user._id,
     email: user.email,
-    balance: user.balance,
+    balance: updatedUser.balance,
     acces_token: user.acces_token,
     refresh_token: user.refresh_token,
   };
